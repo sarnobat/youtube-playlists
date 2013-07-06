0455814863694cc9c3747db86733953d6938eaa8
Sridhar Sarnobat <ss401533@gmail.com>
2013-07-05 22:59:59 -0700

    work in progress

--- list_works.sh
+++ list_works.sh
@@ -1,3 +1,4 @@
 # This requires a SERVER key, that is created here:
 # https://code.google.com/apis/console/#project:100917758076:access
-curl -H "X-JavaScript-User-Agent: www.googleapis.com" "https://www.googleapis.com/youtube/v3/playlists?part=id,snippet,status&channelId=UCfy_YNMOpOQyXnu7C_qtPMA&maxResults=50&key=AIzaSyDrVuKpzAesWaxmtPvrh_gvhm-035y2aWc"
+curl -H "X-JavaScript-User-Agent: www.googleapis.com" \
+	"https://www.googleapis.com/youtube/v3/playlists?part=id,snippet,status&channelId=UCfy_YNMOpOQyXnu7C_qtPMA&maxResults=50&key=AIzaSyDrVuKpzAesWaxmtPvrh_gvhm-035y2aWc"
