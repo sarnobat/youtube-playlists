6c8073502b99b15c428bdfa1837f655e1adc63c2
Sridhar Sarnobat <ssarnobat@control4.com>
2018-01-19 21:59:58 -0800

    Minor changes (untested)

---
+++ run.sh
@@ -0,0 +1,2 @@
+sh youtube_playlists.sh | jq -r '.items[].id' | tee youtube_playlists_output.txt
+cat youtube_playlists_output.txt  |  xargs --delimiter '\n' --max-args=1 -IPLAYLIST_ID curl -i -G -d "playlistId="PLAYLIST_ID"&maxResults=25&part=snippet%2CcontentDetails&key=AIzaSyDrVuKpzAesWaxmtPvrh_gvhm-035y2aWc" https://www.googleapis.com/youtube/v3/playlistItems | tee youtube_playlist_items_output.txt
