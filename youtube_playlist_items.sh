74ca8d6b729943a61c7de7bd1e5e15e891f9fd0c
Sridhar Sarnobat <ssarnobat@control4.com>
2018-01-19 20:56:00 -0800

    Minor changes (untested)

---
+++ youtube_playlist_items.sh
@@ -0,0 +1,2 @@
+
+curl -i -G -d "playlistId=PLBCF2DAC6FFB574DE&maxResults=25&part=snippet%2CcontentDetails&key=AIzaSyDrVuKpzAesWaxmtPvrh_gvhm-035y2aWc" https://www.googleapis.com/youtube/v3/playlistItems
