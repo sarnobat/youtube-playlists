9da56290ed65c4a903a7e5fd4a7605758efdadee
Sridhar Sarnobat <ss401533@gmail.com>
2022-11-06 22:46:50 -0800

    Minor changes (untested)

--- youtube_playlists.sh
+++ youtube_playlists.sh
@@ -10,7 +10,16 @@
 #
 # REFERENCE
 # 	https://developers.google.com/youtube/v3/docs/playlists/list#try-it
+set -e
+
+######################
+# https://console.cloud.google.com/apis/credentials?project=cmdg-2021-08
+# API key 2
+# "Your API key": AIza.....
+test -n "$KEY" || echo "KEY not set (do not source control)"
+test -n "$KEY" || exit
+######################
 
 # This is easy since it only uses public data. Trying to use the GMail API is a lot of work.
 curl --insecure -H "X-JavaScript-User-Agent: www.googleapis.com" \
-	"https://www.googleapis.com/youtube/v3/playlists?part=id,snippet,status&channelId=UCfy_YNMOpOQyXnu7C_qtPMA&maxResults=50&key=<DO NOT SOURCE CONTROL>" | tee playlists.json
+	"https://www.googleapis.com/youtube/v3/playlists?part=id,snippet,status&channelId=UCfy_YNMOpOQyXnu7C_qtPMA&maxResults=50&key=$KEY" | tee playlists.json
