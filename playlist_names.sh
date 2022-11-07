1cb50e9abaaf0693ce470c8b3185b3c4f9d5f622
Sridhar Sarnobat <ss401533@gmail.com>
2022-11-06 23:02:02 -0800

    Minor changes (untested)

--- playlist_names.sh
+++ playlist_names.sh
@@ -1,3 +1,3 @@
 # cat playlists.json | jq '.items[].snippet.title' | tee playlist_names.sh.out
 #cat playlists.json | jq -r '.items[] | .snippet.playlistId +  "\thttps://www.youtube.com/playlist?list=" + .snippet.id + "\t"  + "\t" + .snippet.title' | tee  playlist_names.sh.out
-cat playlists.json | jq -r '.items[] | "https://www.youtube.com/playlist?list=" + .id + "\t" + .snippet.title' | tee  playlist_names.sh.out
+cat playlists.json | jq -r '.items[] | "https://www.youtube.com/playlist?list=" + .id + "\t" + .snippet.title' | sort --key 2 --ignore-case | tee  playlist_names.sh.out
